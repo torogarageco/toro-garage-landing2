37597fa70d6bacb2af79431e06211d1a5179a89e
torogarageco <torogarageco@gmail.com>
2026-06-24 11:12:02 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,15 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'galaxy-prod.tlcdn.com',
+        protocol: "https",
+        hostname: "galaxy-prod.tlcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
